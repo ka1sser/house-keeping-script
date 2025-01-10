ebaebc8e7aa8f49e639eff8848b91df86f4412fd
ka1sser <serenokaisser.dev@gmail.com>
2025-01-10 16:18:54 +0800

    Extracting the dir, number_of_days, and pattern done. Matching the file names ongoing

--- cleaning.py
+++ cleaning.py
@@ -5,21 +5,42 @@ old from the current date.
 
 import os
 import yaml
+from datetime import datetime, timedelta
+import re
 
 with open("./cleaning_config.yaml", "r") as file:
     config = yaml.safe_load(file)
 
-folder = []
+folders = []
 number_of_days = []
-pattern = []
+patterns = []
 
 for dict in config["input_paths"]:
     for key in dict.keys():
         if key == "dir":
             filename = dict.get(key)
-            folder.append(os.path.abspath(filename))
+            folders.append(os.path.abspath(filename))
         if key == "number_of_days":
             number_of_days.append(dict.get(key))
         if key == "pattern":
-            pattern.append(dict.get(key))
+            patterns.append(dict.get(key))
 
+current_date = datetime.now()
+formatted_current_date = current_date.strftime("%Y%m%d")
+
+for folder in folders:
+
+    folder_index = folders.index(folder)
+    
+    old_date = current_date - timedelta(days=number_of_days[folder_index])
+    formatted_old_date = old_date.strftime("%Y%m%d")
+
+    pattern = f"r'{patterns[folder_index]}'"
+
+    for filename in os.listdir(folder):
+        match = re.match(pattern, filename)
+        
+        if match:
+
+            date_str = match.group(1)
+            print(date_str)
