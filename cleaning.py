295219b7170f5c523187cc58ef705030adcda82e
ka1sser <serenokaisser.dev@gmail.com>
2025-01-14 12:02:12 +0800

    Refactored the code into functions and added if __name__ == "__main__" idiom

--- cleaning.py
+++ cleaning.py
@@ -10,70 +10,92 @@ import glob
 import logging
 from logging.handlers import TimedRotatingFileHandler
 
-with open("./cleaning_config.yaml", "r") as file:
-    config = yaml.safe_load(file)
+def load_yaml():
+    with open("./cleaning_config.yaml", "r") as file:
+        config = yaml.safe_load(file)
+    return config
 
-output_file_path = config["log_file"]["path"]
-log_dir = os.path.abspath(output_file_path)
+def check_path(log_dir):
+    if not os.path.exists(log_dir):
+        os.makedirs(log_dir)
 
-if not os.path.exists(log_dir):
-    os.makedirs(log_dir)
+def load_folders(config):
+    folders = []
+    for dict in config["input_paths"]:
+        for key in dict.keys():
+            if key == "dir":
+                folders.append(dict.get(key))
+    return folders
 
-log_file_path = os.path.join(log_dir, "app.log")
+def load_number_of_days(config):
+    number_of_days = []
+    for dict in config["input_paths"]:
+        for key in dict.keys():
+            if key == "number_of_days":
+                number_of_days.append(dict.get(key))
+    return number_of_days
 
-time_logger = logging.getLogger("TimeBasedLogger")
-time_logger.setLevel(logging.INFO)
+def load_patterns(config):
+    patterns = []
+    for dict in config["input_paths"]:
+        for key in dict.keys():
+            if key == "pattern":
+                patterns.append(dict.get(key))
+    return patterns
 
-#Change when and interval depending on the need
-time_handler = TimedRotatingFileHandler(log_file_path, when="s", interval=5, backupCount=7)
-time_handler.suffix = "%Y-%m-%d_%H-%M-%S.log"
-formatter = logging.Formatter("%(asctime)s - %(levelname)s - %(message)s")
-time_handler.setFormatter(formatter)
-time_logger.addHandler(time_handler)
+def main():
+    config = load_yaml()
 
-folders = []
-number_of_days = []
-patterns = []
+    current_date = datetime.now()
+    formatted_current_date = current_date.strftime("%Y%m%d")
 
-for dict in config["input_paths"]:
-    for key in dict.keys():
-        if key == "dir":
-            filename = dict.get(key)
-            folders.append(os.path.abspath(filename))
-        if key == "number_of_days":
-            number_of_days.append(dict.get(key))
-        if key == "pattern":
-            patterns.append(dict.get(key))
+    folders = load_folders(config)
+    number_of_days = load_number_of_days(config)
+    patterns = load_patterns(config)
 
-current_date = datetime.now()
-formatted_current_date = current_date.strftime("%Y%m%d")
+    for folder in folders:
 
-for folder in folders:
+        folder_index = folders.index(folder)
+        
+        old_date = current_date - timedelta(days=number_of_days[folder_index])
+        formatted_old_date = old_date.strftime("%Y%m%d")
 
-    folder_index = folders.index(folder)
+        pattern = patterns[folder_index]
+
+        if not os.listdir(folder):
+                time_logger.info(f"Checking {folder}...")
+                time_logger.warning(f"Folder is empty!")
+                time_logger.info("Skipping...\n")
+        
+        else:
+
+            for filename in os.listdir(folder):
+                files = glob.glob(os.path.join(folder, pattern))
+                
+                for file in files:
+                    if os.path.join(folder,filename) not in files:
+                        time_logger.warning(f"{filename} does not match the [{pattern}] pattern.")
+                        time_logger.info("Skipping...\n")
+                    else:
+                        time_logger.info(f"{os.path.basename(file)} matches the [{pattern}] pattern.")
+                        time_logger.warning(f"Deleting file: {os.path.basename(file)}")
+                        time_logger.info("File successfully deleted.\n")
+                        os.remove(file)
+
+if __name__ == "__main__":
+    time_logger = logging.getLogger("TimeBasedLogger")
+    time_logger.setLevel(logging.INFO)
+
+    config = load_yaml()
+    log_dir = config["log_file"]["path"]
+    check_path(log_dir)
+    log_file_path = os.path.join(log_dir, "app.log")
+    #Change when and interval depending on the need
+    time_handler = TimedRotatingFileHandler(log_file_path, when="s", interval=5, backupCount=7)
+    time_handler.suffix = "%Y-%m-%d_%H-%M-%S.log"
+    formatter = logging.Formatter("%(asctime)s - %(levelname)s - %(message)s")
     
-    old_date = current_date - timedelta(days=number_of_days[folder_index])
-    formatted_old_date = old_date.strftime("%Y%m%d")
-
-    pattern = patterns[folder_index]
-
-    if not os.listdir(folder):
-            time_logger.info(f"Checking {folder}...")
-            time_logger.warning(f"Folder is empty!")
-            time_logger.info("Skipping...\n")
+    time_handler.setFormatter(formatter)
+    time_logger.addHandler(time_handler)
     
-    else:
-
-        for filename in os.listdir(folder):
-            
-            files = glob.glob(os.path.join(folder, pattern))
-            
-            for file in files:
-                if os.path.join(folder,filename) not in files:
-                    time_logger.warning(f"{filename} does not match the [{pattern}] pattern.")
-                    time_logger.info("Skipping...\n")
-                else:
-                    time_logger.info(f"{os.path.basename(file)} matches the [{pattern}] pattern.")
-                    time_logger.warning(f"Deleting file: {os.path.basename(file)}")
-                    time_logger.info("File successfully deleted.\n")
-                    os.remove(file)
+    main()
